84f9448df719c1ffac3c196aec08835ca4dc8dbd
Justin Ichiro Toyomitsu <ichitsurume@gmail.com>
2020-08-31 16:13:08 -0700

    adding main script to vagrant
    adding script to vagrant

---
+++ link_start/isekai_start.sh
@@ -0,0 +1,9 @@
+#BY: Justin Ichiro Toyomitsu
+#main script to install basic neccessities
+# 1. installing Apache
+# 2. installing a database (MYSQL)
+# 3. 
+
+echo "============ START ISEKAI ============"
+
+echo "============= END ISEKAI ============="
