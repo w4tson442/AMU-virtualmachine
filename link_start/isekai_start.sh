d4a80e49779a44d008a7be313ff66bf33f9c2bcc
Justin Ichiro Toyomitsu <ichitsurume@gmail.com>
2020-09-02 14:08:00 -0700

    weird bug
    weird bug

--- link_start/isekai_start.sh
+++ link_start/isekai_start.sh
@@ -3,19 +3,18 @@
 #main script to install basic neccessities
 # 1. installing Apache
 # 2. installing a database (MYSQL)
-# 3. 
-
 echo "============ START ISEKAI ============"
 # install apache2(web server)
 # link: (https://ubuntu.com/tutorials/install-and-configure-apache#1-overview)
 sudo apt update
+sudo apt upgrade
 sudo apt install -y apache2
-
+sudo service apache2 restart
 #moving check.html into the document root
 sudo cp -r /vagrant/* /home/vagrant
 sudo cp -r /home/vagrant/project_here/* /var/www/html
-
 #install which language(default php)?
-#sh language_installs/php.sh
-#sudo service apache2 restart
+echo "---------- INSTALL LANGUAGE ----------"
+sh language_installs/php_script.sh
+echo "-------------- FINISHED --------------"
 echo "============= END ISEKAI ============="
---
+++ link_start/language_installs/php_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install -y php7.2
