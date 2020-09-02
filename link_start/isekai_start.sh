761e3b51eba5b5f772d83a1931040e63904fa595
Justin Ichiro Toyomitsu <ichitsurume@gmail.com>
2020-09-02 14:44:26 -0700

    i don't know why this works
    for some reaosn it doesn't complain like this

--- link_start/isekai_start.sh
+++ link_start/isekai_start.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
 # BY: Justin Ichiro Toyomitsu
 #main script to install basic neccessities
 # 1. installing Apache
 # 2. installing a database (MYSQL)
 echo "============ START ISEKAI ============"
+
 # install apache2(web server)
 # link: (https://ubuntu.com/tutorials/install-and-configure-apache#1-overview)
+sudo apt install -y apache2 mysql-server
+
 sudo apt update
 sudo apt upgrade
-sudo apt install -y apache2
-sudo service apache2 restart
+
+sudo apt install -y git
+sudo apt install -y curl
+
 #moving check.html into the document root
 sudo cp -r /vagrant/* /home/vagrant
 sudo cp -r /home/vagrant/project_here/* /var/www/html
-#install which language(default php)?
+
+sudo service apache2 restart
+
 echo "---------- INSTALL LANGUAGE ----------"
 sh language_installs/php_script.sh
 echo "-------------- FINISHED --------------"
+
 echo "============= END ISEKAI ============="
--- link_start/language_installs/php_script.sh
+++ link_start/language_installs/php_script.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 sudo apt-get install -y php7.2
