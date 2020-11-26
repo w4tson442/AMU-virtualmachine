911b7bd9cecf2d4d9b395b958d13d16bcd8e1333
JustinToyomitsu <w4tson442@gmail.com>
2020-11-25 16:06:57 -0800

    Django Works with Apache Now, (First part of the django application is working)

--- link_start/language_installs/python/script.sh
+++ link_start/language_installs/python/script.sh
@@ -1,11 +1,12 @@
+#Required so that python is python3
 sudo ln -s /usr/bin/python3 /usr/bin/python
-sudo apt-get install -y python3-pip
-sudo pip3 install pymysql
 
-#To make this work with Django
-sudo pip3 install django
-sudo apt-get install libapache2-mod-wsgi-py3
+#Installing required library for running Django
+sudo apt-get install -y python3-pip libapache2-mod-wsgi-py3
+sudo pip3 install pymysql
 sudo pip3 install virtualenv
 
-sudo rm /etc/apache2/sites-enabled/000-default.conf
-sudo cp language_installs/python/000-default.conf /etc/apache2/sites-enabled/
+#sudo pip3 install django
+
+#sudo rm /etc/apache2/sites-enabled/000-default.conf
+#sudo cp language_installs/python/000-default.conf /etc/apache2/sites-enabled/
